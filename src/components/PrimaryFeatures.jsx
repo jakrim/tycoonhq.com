68eaa8e54f1cf42bb8d9630d605892edfb967a50
Jesse Krim <jesse.krim@gmail.com>
2022-10-12 10:23:13 -0400

    Minor

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -179,7 +179,7 @@ function ExploreScreen({ custom, animated = false }) {
       </MotionAppScreenHeader>
       <MotionAppScreenBody {...(animated ? { ...bodyAnimation, custom } : {})}>
         <div>
-          <Image src={'ExplorePage.png'} alt='explore page' width={320} height={100}/>
+          <Image src={require('/ExplorePage.png')} alt='explore page' width={320} height={100}/>
           {/* <div className="space-y-6">
             {[
               { label: 'Full name', value: 'Albert H. Wiggin' },
