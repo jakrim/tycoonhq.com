7e32fd41b6026cbf233f142b0467e3fb1d96705b
Dave M <dmonahan24@gmail.com>
2023-05-15 12:55:50 -0400

    footer form changes

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -3,7 +3,6 @@ const {Client} = require("@notionhq/client");
 exports.handler = async function (event) {
   const notion = new Client({ auth: process.env.NOTION_TOKEN });
   const form = JSON.parse(event.body).payload.data;
-  console.log(form)
   const new_row = {
     parent: {
       database_id: process.env.NOTION_DATABASE_ID
